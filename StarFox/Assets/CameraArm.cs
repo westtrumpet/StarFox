3d4fcbc979f503008c089f6f9211f1fce8c54bc0
Alexander Westby <west.trumpet@gmail.com>
2016-01-28 22:17:49 -0600

    First push. Game setup, player setup, network setup, rudimentary controls, low-res ship model, reticle

---
+++ StarFox/Assets/CameraArm.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraArm : MonoBehaviour {
+	
+	public Camera playerCam;
+	public Rigidbody playerRigid;
+	public float topY;
+	public float botY;
+	public float frontZ;
+	public float backZ;
+	public float midZ;
+
+	public float angX;
+	public float angY;
+	public float angZ;
+
+	void Update () {
+		angX = playerRigid.angularVelocity.x;
+		angY = playerRigid.angularVelocity.y;
+		angZ = playerRigid.angularVelocity.z;
+
+		
+	}
+}
---
+++ StarFox/Assets/scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public GameObject player;
+	//public Color playerColor;
+	bool onMobile;
+	public Rigidbody rigid;
+	public float speed;
+	public float rotateSpeed;
+	//public ParticleSystem particles;
+	GameObject mainCam;
+
+	void Start() {
+		if (SystemInfo.deviceType == DeviceType.Handheld) {
+			Input.gyro.enabled = true;
+			onMobile = true;
+		}
+		else {
+			onMobile = false;
+		}
+		rigid.velocity = speed * Vector3.forward;
+		//particles.startColor = playerColor;
+		//particles.Play();
+		mainCam = GameObject.FindGameObjectWithTag("MainCamera");
+		mainCam.SetActive(false);
+	}
+
+	void Update() {
+		if(onMobile) {
+			player.transform.rotation = Input.gyro.attitude;
+		}
+		else {
+			float horiz = Input.GetAxis("Horizontal");
+			float vert = -1 * Input.GetAxis("Vertical");
+			rigid.AddTorque((horiz * rotateSpeed * player.transform.up + vert * rotateSpeed * player.transform.right), ForceMode.VelocityChange);
+			//player.transform.Rotate(new Vector3(vert * rotateSpeed, horiz * rotateSpeed, 0.0f));
+			rigid.velocity = speed * player.transform.forward;
+		}
+	}
+
+	void OnDestroy(){
+		mainCam.SetActive(true);
+		Destroy(player);
+	}
+}
